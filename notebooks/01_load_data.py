18862cb6f9243dff835efedc786b8fde35945cb9
Atakan <atakanavseveer@gmail.com>
2025-09-21 01:13:00 +0300

    Update 01_load_data.py

--- notebooks/01_load_data.py
+++ notebooks/01_load_data.py
@@ -4,34 +4,34 @@ import os
 
 
 
-# Dosya yolunu ayarla
+#Dosya yolunu ayarla
 csv_path = Path("data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-# CSV'yi oku
+#CSV'yi oku
 df = pd.read_csv(csv_path)
 
 
-# TotalCharges sayısal hale getir
+#TotalCharges sayısal hale getir
 df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
 
-# Hatalı olanları (NaN) at
+#Hatalı olanları NaN at
 print("NaN sayısı (TotalCharges):", df["TotalCharges"].isna().sum())
 df = df.dropna(subset=["TotalCharges"])
 
 
-# İlk bakış
+#ilk görüntü
 print("Şekil:", df.shape)
 print("Kolonlar:", df.columns.tolist())
 print(df.head())
 
-# Eksik değer kontrolü
+#eksik degerr control
 print("\nEksik değer sayıları:")
 print(df.isna().sum())
 
-# Hedef değişkeni (Churn) Yes/No -> 1/0 yap
+#hedef değişkeni (Churn) Yes/No - 1/0 yap
 df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
 
-# Kaydet
+#save
 out_path = Path("data/processed/telco_clean.csv")
 out_path.parent.mkdir(parents=True, exist_ok=True)
 df.to_csv(out_path, index=False)
